d7d64d00efe929d7276337f45b5d56a487247311
Shariif-Mohsoo <mr8781256@gmail.com>
2025-10-15 17:31:47 +0500

    Dice App done

--- testApp2/app/src/main/java/com/example/testapp2/MainActivity.java
+++ testApp2/app/src/main/java/com/example/testapp2/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.testapp2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+            Button rollButton = findViewById(R.id.Roll_btn);
+            final ImageView leftDice = findViewById(R.id.image_leftDice);
+            final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+            final int [] diceArray={
+                    R.drawable.dice_1,
+                    R.drawable.dice_2,
+                    R.drawable.dice_3,
+                    R.drawable.dice_4,
+                    R.drawable.dice_5,
+                    R.drawable.dice_6
+            };
+
+            rollButton.setOnClickListener(new View.OnClickListener(){
+               public void onClick(View v){
+                   Random rand = new Random();
+                   int num1 = rand.nextInt(6);
+                   int num2 = rand.nextInt(6);
+                   leftDice.setImageResource(diceArray[num1]);
+                   rightDice.setImageResource(diceArray[num2]);
+               }
+            });
     }
 }
