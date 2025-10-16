e4b34255360365a19e9586f2962f164369307837
Shariif-Mohsoo <mr8781256@gmail.com>
2025-10-16 20:27:52 +0500

    Sample task

---
+++ testApp3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+            TextView textView;
+            textView = findViewById(R.id.text1);
+            ImageView imageView;
+            imageView = findViewById(R.id.img);
+            Button btn;
+            btn = findViewById(R.id.btn1);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("Button Clicked");
+                }
+            });
+    }
+}
