1052b579c083f1ed09c86c24e27712a5793aa0fc
Shariif-Mohsoo <mr8781256@gmail.com>
2025-11-21 20:35:23 +0500

    Practice

--- testApp6/app/src/main/java/com/example/testapp6/MainActivity2.java
+++ testApp6/app/src/main/java/com/example/testapp6/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.testapp6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,52 @@ public class MainActivity2 extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btn = findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
             return insets;
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("onStart", "onStart: OnStart of activity 2");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume", "onResume: onResume of activity 2");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("onPause", "onPause: onPause of activity 2");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("onStop", "onStop: onStop of activity 2");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("onRestart", "onRestart: onRestart of activity 2");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("onDestroy", "onDestroy: onDestroy of activity 2");
     }
 }
